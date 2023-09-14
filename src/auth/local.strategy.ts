2c173dc7ed39b6a6abd7c50f82f612da9f822de7
priyankambliya18@gmail.com <priyank.a@elaunchinfotech.in>
2023-09-14 11:56:32 +0530

    chron add file passport local-strategy

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Strategy } from "passport-local";
+import { AuthService } from "./auth.service";
+import { PassportStrategy } from "@nestjs/passport"
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super(
+      {
+        usernameField: 'email',
+        passwordField: 'password'
+      }
+    );
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
